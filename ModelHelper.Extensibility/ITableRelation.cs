23a48e077c5d20b1d23b54112aeecd51a6aaec12
Hans-Petter Eitvet <hans-petter@patogen.no>
2018-12-20 15:38:01 +0100

    feat: adds --traverse-relations to list a complete tree of relations for a given table

--- ModelHelper.Extensibility/ITableRelation.cs
+++ ModelHelper.Extensibility/ITableRelation.cs
@@ -11,4 +11,27 @@
 
         bool IsSelfJoin { get; set; }
     }
+
+    public interface IRelation
+    {
+        int SortIndex { get; set; }
+        int Level { get; set; }
+
+        string FullPath { get; set; }
+        int Depth { get; set; }
+
+        string Family { get; set; }
+
+        string ReferenceName { get; set; }
+
+        string ParentTableName { get; set; }
+        string ParentColumnName { get; set; }
+        string ParentColumnType { get; set; }
+
+        string ReferencedTableName { get; set; }
+        string ReferencedColumnName { get; set; }
+        string ReferencedColumnType { get; set; }
+
+        bool IsSelfJoin { get; set; }
+    }
 }
