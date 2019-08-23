93d732229041350df97372faa4c3867166caf4cd
hp-e <hpephoto@gmail.com>
2019-08-23 09:47:26 +0200

    remove obsolete code

--- ModelHelper.Core/Project/ProjectApiModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ModelHelper.Core.Project
-{
-    public class ProjectApiModel
-    {
-        public bool UseLogger { get; set; }
-        public bool UseTelemetry { get; set; }
-        public bool UseUserRepository { get; set; }
-    }
-}
